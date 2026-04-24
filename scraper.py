6ceb45bc9e620a7c75e9f6915737dbaf8c0a03c5
pvtrycjv <romanazupa131@gmail.com>
2026-04-24 22:42:02 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -207,4 +207,4 @@ def add_from_url():
 # ---------------- MAIN ---------------- #
 if __name__ == "__main__":
     init_db()
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=10000)
